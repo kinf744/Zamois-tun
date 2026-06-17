2e181eb5b643d3d955c7949e0f065430c58aca27
kinf744 <adrienyuoi@gmail.com>
2026-06-17 06:22:49 +0100

    fix: multi-profil V2ray+DNS - toujours utiliser le balancier SOCKS au lieu de startMulti

--- app/src/main/java/com/kighmu/vpn/engines/MultiXraySlowDnsEngine.kt
+++ app/src/main/java/com/kighmu/vpn/engines/MultiXraySlowDnsEngine.kt
@@ -328,16 +328,9 @@ class MultiXraySlowDnsEngine(
             return
         }
         if (HevTun2Socks.isAvailable) {
-            val ports = activePorts.filter { it > 0 }
-            if (ports.size > 1) {
-                // Multi-SOCKS natif dans hev - plus fiable que le balancier Kotlin
-                KighmuLogger.info(TAG, "hev V2ray+DNS -> multi-SOCKS natif ports=$ports")
-                HevTun2Socks.startMulti(context, fd, ports, svc, mtu = 8500)
-            } else {
-                val targetPort = ports.firstOrNull() ?: (if (balancerPort > 0) balancerPort else SocksBalancer.BALANCER_PORT)
-                KighmuLogger.info(TAG, "hev V2ray+DNS -> single SOCKS port=$targetPort")
-                HevTun2Socks.start(context, fd, targetPort, svc)
-            }
+            val targetPort = SocksBalancer.BALANCER_PORT
+            KighmuLogger.info(TAG, "hev V2ray+DNS -> balancer:$targetPort (${activePorts.size} tunnel(s))")
+            HevTun2Socks.start(context, fd, targetPort, svc)
         } else {
             xrayEngines.firstOrNull()?.startTun2Socks(fd)
         }
